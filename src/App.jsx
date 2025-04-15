0737cc694b4fd62e0f77ac3f0df6dc27320bebf0
Priyanka <priyankagjune@gmail.com>
2025-04-14 18:16:45 -0600

    inmplemented increment and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    if (count >= 0) {
+      setCount(count + 1);
+    }
+  };
 
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <div className="container">
-      <button className="btn" onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={handleIncrement}>
         +
       </button>
       Count: {count}
-      <button className="btn" onClick={() => setCount(count - 1)}>
+      <button className="btn" onClick={handleDecrement}>
         -
       </button>
     </div>
